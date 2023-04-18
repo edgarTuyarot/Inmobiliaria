5c64bd31e1715cc075b33428cf3b6e5fb455b26c
Edgar <edgar.m.tuyarot@hotmail.com>
2023-04-18 16:17:39 -0300

    Desarrollo de la funcion

--- src/js/agregarImagen.js
+++ src/js/agregarImagen.js
@@ -1,3 +1,37 @@
 import {Dropzone} from 'dropzone'
 
-alert("work!")
+const token = document.querySelector('meta[name="csrf-token"]').content
+
+//Desde aca se configura el dropzone
+Dropzone.options.imagen = {
+    dictDefaultMessage: "sube tus imagenes aqui",
+    acceptFiles: '.png,.jpg,.jpeg',
+    maxFilesize: 5,
+    maxFiles: 1,
+    parallelUploads:1,
+    autoProcessQueue:false,
+    addRemoveLinks:true,
+    dictRemoveFile:'Borrar imagen',
+    dictMaxFilesExceeded:"el limite es 1 archivo",
+    dictFileTooBig:"Tamaño maximo 5MB",
+    headers:{
+        'CSRF-Token': token
+    },
+    paramName:'imagen',
+    //modificando funciones de dropzone
+    init: function (){
+        const dropzone = this
+        //asiganr la funcion al boton publicar
+        const btnPublicar =  document.querySelector('#publicar')
+        btnPublicar.addEventListener('click', function(){
+            dropzone.processQueue()
+        })
+        //asignar la redireccion
+        dropzone.on('queuecomplete',function(){
+            if(dropzone.getActiveFiles().length == 0){
+                window.location.href = '/mis-propiedades'
+            }
+        })
+    }
+
+}
